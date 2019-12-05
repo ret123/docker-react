bccd8750a69c0805ae70f36ac62aa1081aed5621
ret123 <rathish83@gmail.com>
2019-12-05 17:52:51 +0530

    Changed app.js content

--- src/App.js
+++ src/App.js
@@ -8,16 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hi From React!!!!
+         Hi From React!!!! Changed text for feature branch!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
       </header>
     </div>
   );

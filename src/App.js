e9de0d7422e7fc83d6664664e06cbeaf5f9fdd0b
ret123 <rathish83@gmail.com>
2019-12-05 18:16:46 +0530

    Changes made to app.js for test success

--- src/App.js
+++ src/App.js
@@ -8,9 +8,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hi From React!!!! Changed text for feature branch!
+          Edit <code>src/App.js</code> and save to reload.
         </p>
-      
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
